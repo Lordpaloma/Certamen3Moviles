ac664d00b583cd3839245a4211494e6d8ec81386
JOlave <jaime.olave@usm.cl>
2024-07-10 23:23:01 -0400

    Glep
    Clientes vista, con codigo d kpeli (problemas con git :c )

--- certamen3/lib/pages/tabs/clientes.dart
+++ certamen3/lib/pages/tabs/clientes.dart
@@ -1,4 +1,6 @@
+import 'package:certamen3/services/firestore_service.dart';
 import 'package:certamen3/widgets/tile_clienteres.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Clientes extends StatefulWidget {
@@ -9,7 +11,7 @@ class Clientes extends StatefulWidget {
 }
 
 class _ClientesState extends State<Clientes> {
-  final AssetImage fondo = AssetImage('assets/images/fondo.png');
+  final AssetImage fondo = AssetImage('assets/images/hola.png');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +20,66 @@ class _ClientesState extends State<Clientes> {
           image: DecorationImage(image: fondo, fit: BoxFit.cover),
         ),
         padding: EdgeInsets.all(10),
+        child: StreamBuilder(
+          stream: FirestoreService().clientes(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData ||
+                snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+                            return ListView.separated(
+                separatorBuilder: (context, Index) => Divider(),
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var cliente = snapshot.data!.docs[index];
+                  return ListBody(
+                    children: [
+                      TilesClientesResumen(nombre: cliente['nombre'], problema: cliente['problema'],)
+                    ],
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
+    );
+  }
+
+  void mostrarInfoClientes(BuildContext context, cliente) {
+    showAdaptiveDialog(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: 350,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(96, 29, 1, 73),
+              border:
+                  Border.all(color: Color.fromARGB(255, 29, 1, 73), width: 2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            child: Column(children: [
+              Spacer(),
+              Container(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(115, 66, 1, 207)),
+                    child: Text('Cerrar'),
+                    onPressed: () => Navigator.pop(context),
+                  ))
+            ]),
+          ),
+        );
+      },
     );
   }
 }

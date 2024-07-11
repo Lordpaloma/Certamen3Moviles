86c4060115492c82612c78e8b18b79b020efb9ae
JOlave <jaime.olave@usm.cl>
2024-07-10 21:19:22 -0400

    PIM

--- certamen3/lib/main.dart
+++ certamen3/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -32,6 +33,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.black45,
+          centerTitle: true
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- certamen3/lib/pages/tabs/smilingfriends.dart
+++ certamen3/lib/pages/tabs/smilingfriends.dart
@@ -1,10 +1,57 @@
+import 'package:certamen3/services/firestore_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 
-class SmilingFriends extends StatelessWidget {
+class SmilingFriends extends StatefulWidget {
   const SmilingFriends({super.key});
 
   @override
+  State<SmilingFriends> createState() => _SmilingFriendsState();
+}
+
+class _SmilingFriendsState extends State<SmilingFriends> {
+  @override
+  final AssetImage fondo = AssetImage('assets/images/hola.png');
+
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: fondo, fit: BoxFit.cover),
+        ),
+        padding: EdgeInsets.all(10),
+        child: StreamBuilder(
+          stream: FirestoreService().trabajadores(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData ||
+                snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return ListView.separated(
+                separatorBuilder: (context, Index) => Divider(),
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var trabajador = snapshot.data!.docs[index];
+                  return ListBody(
+                    children: [Container(
+                      margin: EdgeInsets.all(10),
+                      color: Colors.black45,
+                      width: 100,
+                    )],
+                  );
+                  //                   leading: Icon(MdiIcons.emoticonHappy),
+                  //                 title: Text('${trabajador['nombre']} ${trabajador['apellido']}'),
+                  //               subtitle: Text('Edad:${trabajador['edad']}'),
+                  //           );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
